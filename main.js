d565cf33f3629c75a97689a92b924051f4684443
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-01 21:20:37 -0500

    Area rectangulo

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let b = Number(prompt("ingrese la base del rectangulo"));
+let h = Number(prompt("ingrese la altura del rectangulo"));
+
+function formulaAreaRectangulo(base,altura){
+    return base * altura;
+}
+
+console.log(`el area del rectangulo es: ${formulaAreaRectangulo(b, h)}`);
+
+

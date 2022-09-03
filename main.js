a07d4a68e319a143dad1ba3089a8f8ef70beabc7
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-03 08:51:03 -0500

    formula para hallar área del círculo

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ let r = Number(prompt(`ingresa el radio`))
 
 
 
-function fomulaAreaCirculo(radio){
-    return Math.pow(radio,2);
+function fomulaAreaCirculo(radio,perimetro){
+    return Math.pow(radio,2)*perimetro;
 }
 
-console.log(`el Area del circulo es: ${fomulaAreaCirculo(r)}`);
+console.log(`el Area del circulo es: ${fomulaAreaCirculo(r,pi)}`);
 
 
